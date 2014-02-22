bb44d4a08fa606c1777aec15edac212fad6004dd
KashMonies <80404@41cs804-04.cooper.edu>
2014-02-22 15:20:17 -0500

    add timer

--- clock.js
+++ clock.js
@@ -8,7 +8,7 @@ function narrate() {
 
 	for(var i = 0; i < chapter.length; i++) {
 		
-		if(chapter[i].number <= sequence) {
+		if(chapter[i].number * 4 <= sequence ) {
 			
 			document.querySelector(".chapter").innerHTML=chapter[i].number;
 			document.querySelector(".text").innerHTML=chapter[i].text;
@@ -16,7 +16,7 @@ function narrate() {
 		}
 	}
 
-	if (ss > 24) {
+	if (ss < 12 || ss > 16) {
       
       document.querySelector(".telstar").style.setProperty("-webkit-transform","rotate("+ (degr) +"deg)", null);
 
@@ -41,87 +41,101 @@ onload = function() { narrate(); }
 var chapter = [
 
 			{ 
-	 			'number' : 1,  
+	 			'number' : 1, 
+	 			'timer' : 4,
 	 			'text' : "Amidst the metallic scent of impending space dollars, 1000 bespectacled operators buzzed in the distinct air of a Cape Canaveral control room."
 	 			     
 	 		}, 
 	
 
 			{ 
-				'number' : 2,  
+				'number' : 2, 
+				'timer' : 8, 
 	 			'text' : "The perspiration of that historical moment became sterilized upon hitting the immaculate surface of NASA grade plastic. There would be no room for error in the specifically grotesque form of human sweat."
 	 			
 			},   
 
 
 			{ 
-				'number' : 3,  
+				'number' : 3,
+				'timer' : 12,  
 	 			'text' : "Across the flattened fold of space-time, one anxious nymph sped with light-footed fear after having been hit by an electrostatic arrow of all encompassing disdain."
 			}, 
 
 
 			{ 
 				'number' : 4,  
+				'timer' : 16,
 	 			'text' : "Meanwhile, in 1985 Richard Feynman drew the connecting lines, feverishly across the muddy green of a chalkboard that had at any given time bore the bits and pieces of an encyclopedia of electro surveillance."
 			}, 
 
 
 			{ 
-				'number' : 5,  
+				'number' : 5, 
+				'timer' : 20, 
 	 			'text' : "The agency of the launch was matched only by the vigor of its utilization. Like a rocket into space, each switch of a living room television sent waves of causality that ricocheted straight through the soft pink undulating substance of human attention."
 	 		}, 
 
 
 	 		{ 
-				'number' : 6,  
+				'number' : 6, 
+				'timer' : 24, 
 	 			'text' : "3 nations of housewives, bachelors, freckled boy scouts, and even scrape-kneed tomboys sat down before the great flickering tube of Telstar, and thus one satellite sponsored the imagination of a globe."
 	 		}, 
 
 
 	 		{ 
 				'number' : 7,  
+				'timer' : 28,
 	 			'text' : "Somewhere, Daphne wrestled with a consciousness split between the thick sappy scent of tree bark and the prickling tingling static of 3 nations worth of Television."
 	 		}, 
 
 
 	 		{ 
 				'number' : 8,  
+				'timer' : 32,
 	 			'text' : "As Apollo awoke to find a laurel wreath strewn lazily across his pounding brow, he leapt up against the rigid flesh of wood that was once his beloved Daphne. Immediately forgetting his amnesia in the caress of a Laurel tree, Apollo's only true contribution to the Daphne/Telstar affair would be the intertwining of consumer space probing and the term 'tree hugger'."
 	 		}, 
 
 
 	 		{ 
 				'number' : 9,  
+				'timer' : 36,
 	 			'text' : "The launch of Telstar, for those who prefer its tale to the hopeless romantic Ovid's, can be traced along the soft white residue of Feynman's moving hand. Retro-Causality, played out on the macro-mythic/market-astrophic scale."
 	 		}, 
 
 
 	 		{ 
 				'number' : 10,  
+				'timer' : 40,
 	 			'text' : "Apollo never did look into that momentary blackout by the Laurel tree, after all he had become quite accustomed to loosing contact with physicality in moments of fickle play, a phenomena to which humans can only ever guess at through extreme adrenaline or orgasm. It was one of those perks of having a return address that read Mt. Olympus."
 	 		}, 
 
 
 	 		{ 
 				'number' : 11,  
+				'timer' : 44,
 	 			'text' : "The fact that Apollo had become reborn in the form of a commercially broadcasting satellite would seem as preposterous to him as telling a positron it is in fact just an electron moving backwards through time."
 	 		},
 
 
 	 		{ 
 				'number' : 12,  
+				'timer' : 48,
 	 			'text' : "This underappreciated fact of nature, is entirely obvious despite Apollo's (and an entire academy of hopeless romantics) ignorance, and is apparent to anyone with common sense and a slightly more fulfilled vantage point along the track of time."
 	 		},
 
 
 	 		{ 
 				'number' : 13,  
+				'timer' : 52,
 	 			'text' : "After all, both Apollo and Telstar share the same track record of partaking in the casual objectification of the female form."
 	 		},
 
 
 	 		{ 
-				'number' : 14,  
+				'number' : 14,
+				'timer' : 56,  
 	 			'text' : "For every flattened woman depicted through the effervescent glow of static electricity, is a freshly choreographed wreath of Laurels, placed in absent care- on the male head of a god."
 	 		},
 
