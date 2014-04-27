11c70bb8728a499669eb8a5c39b8319a6ede8046
Camila Mercado <theronmercado@theron-mercados-macbook-3.local>
2014-04-26 23:48:40 -0400

    add order of events

---
+++ js/4.js
@@ -0,0 +1,158 @@
+
+setInterval(function (turn) {
+    
+    var today = new Date();
+	  var dd = today.getDate();
+    var ss = today.getSeconds();
+    console.log(ss);
+
+    
+    var degr = ss * 6;
+    //console.log(degr);
+
+
+      document.querySelector(".telstar").style.setProperty("-webkit-transform","rotate("+ (degr) +"deg)", null);
+
+    
+
+    if (ss > 39) {
+      
+      daph = ss - 30;
+
+      var daphne = "url(img/daphne/" + daph + ".png)";
+
+      document.querySelector('.sculpture').style.backgroundImage=daphne;
+    }
+    
+    else {
+
+      var daphne = "url(img/daphne/" + ss + ".png)";
+
+      document.querySelector('.sculpture').style.backgroundImage=daphne;
+
+    }
+
+
+}, 1000);
+
+setInterval( function order(){
+
+    for(var i = 0; i < chapter.length; i++) {
+        
+        if(chapter[i].timer === time ) {
+
+          
+          document.querySelector(".chapter").innerHTML=chapter[i].number;
+          document.querySelector(".text4").innerHTML=chapter[i].text;
+
+        }
+
+        if( time >= 260) {
+
+          document.querySelector(".Laurel4").style.display="block";
+
+          setInterval(function () {
+            
+            document.querySelector('.Laurel4').style.opacity="0.3";
+            document.querySelector('.sculpture').style.opacity="0.9";
+          
+          }, 10000);
+
+          setInterval(function () {
+            
+            document.querySelector('.Laurel4').style.opacity="0.6";
+            document.querySelector('.sculpture').style.opacity="0.6";
+          
+          }, 20000);
+
+          setInterval(function () {
+            
+            document.querySelector('.Laurel4').style.opacity="0.9";
+            document.querySelector('.sculpture').style.opacity="0.3";
+          
+          }, 30000);
+
+           setInterval(function () {
+            
+            document.querySelector('.Laurel4').style.opacity="1";
+            document.querySelector('.sculpture').style.opacity="0.1";
+          
+          }, 40000);
+
+
+        }
+
+      }
+
+  }, 1000);
+
+var chapter = [
+
+
+      {
+        'number' : "1)", 
+        'timer' : 200,
+        'text' : "Two figures are hit by Cupids arrow. The god (Apollo), and the nymph (Daphne)."
+         
+      }, 
+
+      { 
+        'number' : "2)",
+        'timer' : 210,  
+        'text' : "Apollo is struck by an unflinching resolve to possess Daphne. Daphne is struck by an unflinching resolve to evade Apollo."
+
+      }, 
+
+      { 
+        'number' : "3)",
+        'timer' : 220,  
+        'text' : "The two figures circle the planet earth in pursuit and evasion. At the end of a third orbit around the globe, Daphne begins to decelerate (feeling an entropy that gods are not acquainted with)."
+
+      }, 
+
+      { 
+        'number' : "4)",
+        'timer' : 230,  
+        'text' : "The decrease in Daphne's speed is a directly proportional inverse relationship to her increase in fear, and Apollo's increase in joy."
+      }, 
+
+      { 
+        'number' : "5)",
+        'timer' : 240,  
+        'text' : "As Daphne's speed/fear ratio continues to tip in favor of terror, she introduces a third figure into the equation, (her father the river god Peneus) by calling for help."     
+      }, 
+
+      { 
+        'number' : "6)",
+        'timer' : 250,  
+        'text' : "The gaze of Peneus pierces through levels of sediment, rock, water and flesh to fix upon the form of the terror stricken nymph."     
+      }, 
+
+      { 
+        'number' : "7)",
+        'timer' : 260,  
+        'text' : "The sprouting of ten fingertips is quickly followed by a hardening of the flesh, a rigidness of the hair, and the thick sappy gag of tree sap. "     
+      }, 
+
+      { 
+        'number' : "8)",
+        'timer' : 270,  
+        'text' : "Apollo looks over the transformation in devastating glee. While Daphne is no longer a nymph, she is now incapable of flight, and defenseless against the realization of Apollo's ever-present will to possess her as his own."     
+      }, 
+
+      { 
+        'number' : "9)",
+        'timer' : 280,  
+        'text' : "Time continues for Daphne, yet now in the form of tree rungs, and a god curled around her limbs. Daphne's figure is fixed in space, yet still desiring to flee."     
+      }, 
+
+      { 
+        'number' : "10)",
+        'timer' : 290,  
+        'text' : "Being so overjoyed at Daphne's newly pacified form, Apollo decides to commemorate his triumph by adopting the Laurel wreath as his personal symbol. He wears the token of Daphne's loss of mobility as an accessory to his power, invites a planets worth of new figures to do the same."     
+      }, 
+
+];
+
+
+
--- js/clock.js
+++ js/clock.js
@@ -3,21 +3,14 @@ function narrate() {
 
 	var today = new Date();
 	var ss = today.getSeconds();
-<<<<<<< HEAD
-	var sequence = ss % 5;
-	var degr = ss * 6;
 
-	for(var i = 0; i < chapter.length; i++) {
-		
-		if(chapter[i].number <= sequence) {
-=======
 	var sequence = ss;
 	var degr = ss - 30;
 
 	for(var i = 0; i < chapter.length; i++) {
 		
 		if(chapter[i].timer <= sequence ) {
->>>>>>> 55f96d186acd8be99b95b85fd3f0679c13648661
+
 			
 			document.querySelector(".chapter").innerHTML=chapter[i].number;
 			document.querySelector(".text").innerHTML=chapter[i].text;
@@ -25,11 +18,7 @@ function narrate() {
 		}
 	}
 
-<<<<<<< HEAD
-	if (ss > 24) {
-=======
 	if (ss < 30) {
->>>>>>> 55f96d186acd8be99b95b85fd3f0679c13648661
       
       document.querySelector(".telstar").style.setProperty("-webkit-transform","rotate("+ (degr) +"deg)", null);
 
@@ -39,11 +28,8 @@ function narrate() {
     
     else {
 
-<<<<<<< HEAD
-      var daphne = "url(img/daphne/" + ss + ".png)";
-=======
       var daphne = "url(img/daphne/" + degr + ".png)";
->>>>>>> 55f96d186acd8be99b95b85fd3f0679c13648661
+
 
       document.querySelector('.sculpture').style.backgroundImage=daphne;
       
@@ -57,93 +43,50 @@ onload = function() { narrate(); }
 
 var chapter = [
 
-			{ 
-<<<<<<< HEAD
-	 			'number' : 1,  
-	 			'text' : "Amidst the metallic scent of impending space dollars, 1000 bespectacled operators buzzed in the distinct air of a Cape Canaveral control room."
-	 			     
-	 		}, 
-	
 
-			{ 
-				'number' : 2,  
-	 			'text' : "The persperation of that historical moment became sterilized upon hitting the immaculate surface of NASA grade plastic. There would be no room for error in the singularly grotesque form of human sweat."
-	 			
-			},   
-
-
-			{ 
-				'number' : 3,  
-	 			'text' : "Across the flattened fold of space time, one anxious nymph, sped with lightfooted fear after having been hit by harbernger tipped arrow of all encompasing disdain."
-			}, 
-
-
-			{ 
-				'number' : 4,  
-	 			'text' : "Meanwhile, in 1985 Richard Feynman drew the connecting lines, fevershely across the muddy green of a chalkboard that had bore the marks of an encycolpedia of the bits and peices of electro surveillance."
-			}, 
-
-
-			{ 
-				'number' : 5,  
-	 			'text' : "The agency of the launch was matched only by the vigor of its utilization. Like a rocket into space, each switch of a living room television sent waves of causality that richoched straight through the soft pink substance of human attention."
-	 		}, 
-
-
-	 		{ 
-				'number' : 6,  
-	 			'text' : "3 nations of housewives, bachelors, freckled boyscouts, and even scrape-kneed tomboys sat down before the great flickering tube of telstar, and thus one satellite sponsored the imagination of a globe."
-=======
+			{
 	 			'number' : 1, 
 	 			'timer' : 1,
 	 			'text' : "Amidst the metallic scent of impending space dollars, 1000 bespectacled operators buzzed in the distinct air of a Cape Canaveral control room. The perspiration of that historical moment became sterilized upon hitting the immaculate surface of NASA grade plastic. There would be no room for error in the specifically grotesque form of human sweat."
 	 			 
 	 		}, 
 
-			// { 
-			// 	'number' : 2,
-			// 	'timer' : 15,  
-	 	// 		'text' : "Across the flattened fold of space-time, one anxious nymph sped with light-footed fear after having been hit by an electrostatic arrow of all encompassing disdain."
-			// }, 
+			{ 
+				'number' : 2,
+				'timer' : 15,  
+	 			'text' : "Across the flattened fold of space-time, one anxious nymph sped with light-footed fear after having been hit by an electrostatic arrow of all encompassing disdain."
+			}, 
 
 
-			// { 
-			// 	'number' : 3,  
-			// 	'timer' : 16,
-	 	// 		'text' : "Meanwhile, in 1985 Richard Feynman drew the connecting lines, feverishly across the muddy green of a chalkboard that had at any given time bore the bits and pieces of an encyclopedia of electro surveillance."
-			// }, 
+			{ 
+				'number' : 3,  
+				'timer' : 16,
+	 			'text' : "Meanwhile, in 1985 Richard Feynman drew the connecting lines, feverishly across the muddy green of a chalkboard that had at any given time bore the bits and pieces of an encyclopedia of electro surveillance."
+			}, 
 
 
-			// { 
-			// 	'number' : 4, 
-			// 	'timer' : 24, 
-	 	// 		'text' : "The agency of the launch was matched only by the vigor of its utilization. Like a rocket into space, each switch of a living room television sent waves of causality that ricocheted straight through the soft pink undulating substance of human attention.3 nations of housewives, bachelors, freckled boy scouts, and even scrape-kneed tomboys sat down before the great flickering tube of Telstar, and thus one satellite sponsored the imagination of a globe."
-	 	// 	}, 
-
-	 	// 	{ 
-			// 	'number' : 5,  
-			// 	'timer' : 32,
-	 	// 		'text' : "Somewhere, Daphne wrestled with a consciousness split between the thick sappy scent of tree bark and the prickling tingling static of 3 nations worth of Television."
-	 	// 	}, 
-
-
-	 	// 	{ 
-			// 	'number' : 6,  
-			// 	'timer' : 40,
-	 	// 		'text' : "As Apollo awoke to find a laurel wreath strewn lazily across his pounding brow, he leapt up against the rigid flesh of wood that was once his beloved Daphne. Immediately forgetting his amnesia in the caress of a Laurel tree, Apollo's only true contribution to the Daphne/Telstar affair would be the intertwining of consumer space probing and the term 'tree hugger'."
-	 	// 	}, 
-
+			{ 
+				'number' : 4, 
+				'timer' : 24, 
+	 			'text' : "The agency of the launch was matched only by the vigor of its utilization. Like a rocket into space, each switch of a living room television sent waves of causality that ricocheted straight through the soft pink undulating substance of human attention.3 nations of housewives, bachelors, freckled boy scouts, and even scrape-kneed tomboys sat down before the great flickering tube of Telstar, and thus one satellite sponsored the imagination of a globe."
+	 		}, 
 
 	 		{ 
-				'number' : 2,  
-				'timer' : 25,
-	 			'text' : "The agency of the launch was matched only by the vigor of its utilization. Like a rocket into space, each switch of a living room television sent waves of causality that ricocheted straight through the soft pink undulating substance of human attention."
->>>>>>> 55f96d186acd8be99b95b85fd3f0679c13648661
+				'number' : 5,  
+				'timer' : 32,
+	 			'text' : "Somewhere, Daphne wrestled with a consciousness split between the thick sappy scent of tree bark and the prickling tingling static of 3 nations worth of Television."
 	 		}, 
 
 
 	 		{ 
-<<<<<<< HEAD
+				'number' : 6,  
+				'timer' : 40,
+	 			'text' : "As Apollo awoke to find a laurel wreath strewn lazily across his pounding brow, he leapt up against the rigid flesh of wood that was once his beloved Daphne. Immediately forgetting his amnesia in the caress of a Laurel tree, Apollo's only true contribution to the Daphne/Telstar affair would be the intertwining of consumer space probing and the term 'tree hugger'."
+	 		}, 
+
+
+	 		{ 
+
 				'number' : 7,  
 	 			'text' : "Somewhere, Daphne wrestled with a conciousness split between the thick sappy scent of tree bark and the prickling tingling static of 3 nations TV sets."
 	 		}, 
@@ -193,88 +136,6 @@ var chapter = [
 
 
 ]
-=======
-				'number' : 3,  
-				'timer' : 45,
-	 			'text' : "3 nations of housewives, bachelors, freckled boy scouts, and even scrape-kneed tomboys sat down before the great flickering tube of Telstar, and thus one satellite sponsored the imagination of a globe."
-	 		},
-
-	 		// { 
-				// 'number' : 9,
-				// 'timer' : 60,  
-	 		// 	'text' : "For every flattened woman depicted through the effervescent glow of static electricity, is a freshly choreographed wreath of Laurels, placed in absent care- on the male head of a god."
-	 		// },
-
-
-
-]
-
-
-// 			{ 
-// 	 			'number' : 1, 
-// 	 			'timer' : 1,
-// 	 			'text' : "Amidst the metallic scent of impending space dollars, 1000 bespectacled operators buzzed in the distinct air of a Cape Canaveral control room. The perspiration of that historical moment became sterilized upon hitting the immaculate surface of NASA grade plastic. There would be no room for error in the specifically grotesque form of human sweat."
-	 			 
-// 	 		}, 
-
-// 			{ 
-// 				'number' : 2,
-// 				'timer' : 8,  
-// 	 			'text' : "Across the flattened fold of space-time, one anxious nymph sped with light-footed fear after having been hit by an electrostatic arrow of all encompassing disdain."
-// 			}, 
-
-
-// 			{ 
-// 				'number' : 3,  
-// 				'timer' : 16,
-// 	 			'text' : "Meanwhile, in 1985 Richard Feynman drew the connecting lines, feverishly across the muddy green of a chalkboard that had at any given time bore the bits and pieces of an encyclopedia of electro surveillance."
-// 			}, 
-
-
-// 			{ 
-// 				'number' : 4, 
-// 				'timer' : 24, 
-// 	 			'text' : "The agency of the launch was matched only by the vigor of its utilization. Like a rocket into space, each switch of a living room television sent waves of causality that ricocheted straight through the soft pink undulating substance of human attention.3 nations of housewives, bachelors, freckled boy scouts, and even scrape-kneed tomboys sat down before the great flickering tube of Telstar, and thus one satellite sponsored the imagination of a globe."
-// 	 		}, 
-
-// 	 		{ 
-// 				'number' : 5,  
-// 				'timer' : 32,
-// 	 			'text' : "Somewhere, Daphne wrestled with a consciousness split between the thick sappy scent of tree bark and the prickling tingling static of 3 nations worth of Television."
-// 	 		}, 
-
-
-// 	 		{ 
-// 				'number' : 6,  
-// 				'timer' : 40,
-// 	 			'text' : "As Apollo awoke to find a laurel wreath strewn lazily across his pounding brow, he leapt up against the rigid flesh of wood that was once his beloved Daphne. Immediately forgetting his amnesia in the caress of a Laurel tree, Apollo's only true contribution to the Daphne/Telstar affair would be the intertwining of consumer space probing and the term 'tree hugger'."
-// 	 		}, 
-
-
-// 	 		{ 
-// 				'number' : 7,  
-// 				'timer' : 48,
-// 	 			'text' : "The agency of the launch was matched only by the vigor of its utilization. Like a rocket into space, each switch of a living room television sent waves of causality that ricocheted straight through the soft pink undulating substance of human attention."
-// 	 		}, 
-
-
-// 	 		{ 
-// 				'number' : 8,  
-// 				'timer' : 56,
-// 	 			'text' : "3 nations of housewives, bachelors, freckled boy scouts, and even scrape-kneed tomboys sat down before the great flickering tube of Telstar, and thus one satellite sponsored the imagination of a globe."
-// 	 		},
-
-// 	 		{ 
-// 				'number' : 9,
-// 				'timer' : 60,  
-// 	 			'text' : "For every flattened woman depicted through the effervescent glow of static electricity, is a freshly choreographed wreath of Laurels, placed in absent care- on the male head of a god."
-// 	 		},
-
-
-
-// ]
->>>>>>> 55f96d186acd8be99b95b85fd3f0679c13648661
-
 
 
 
