cb141e3c22eed9c537fe8e2dbf76cd7341864338
Camila Mercado <camilamercado11@gmail.com>
2014-04-28 19:23:14 -0400

    double slit

--- js/five.js
+++ js/five.js
@@ -12,6 +12,19 @@ setInterval( function order5(){
 
         }
 
+        if( time >= 202) {
+
+          document.querySelector(".board2").style.backgroundImage="url(./img/diffraction.png)";
+
+          setInterval(function () {
+            
+            // document.querySelector('.Laurel4').style.opacity="0.3";
+            // document.querySelector('.sculpture').style.opacity="0.9";
+          
+          }, 10000);
+
+        }
+
       
     }
 
@@ -24,7 +37,7 @@ var chapter5 = [
       {
         'number' : "1)", 
         'timer' : 182,
-        'text' : "An experiment is performed in which a beam of photons or electrons (P/E) is pointed at a series of two boards. The closer board has two rectangular vertical forms removed from the center of the surface like so - [ || ].",
+        'text' : "An experiment is performed in which a beam of photons or electrons (P/E) is pointed at a series of two boards. The closer board has two rectangular vertical forms removed from the center of the surface.",
         'subtext' : "",
         'subtext2' : ""
       }, 
@@ -32,7 +45,7 @@ var chapter5 = [
       { 
         'number' : "2)",
         'timer' : 202,  
-        'text' : "The beam is fired directly at the center point between the holes of the double-slitted board1. A pattern appears on the surface of board2, showing a series of vertical lines, heaviest in thickness at the center of the board, and diminishing sequentially as they approach the edges - [ ||||| ].",
+        'text' : "The beam is fired directly at the center point between the holes of the double-slitted board1. A pattern appears on the surface of board2, showing a series of vertical lines, heaviest in thickness at the center of the board, and diminishing sequentially as they approach the edges.",
         'subtext' : "*This image is recognized as a diffraction pattern (the result of interference between waves). The pattern implies the beam of P/E exhibit properties of wave/particle duality when being sent through the board. In order for this to occur each individual P/E would need to pass through each slit simultaneously, essentially interfering with itself in waveform after passing though board1 to create the diffraction image.",
         'subtext2' : ""
 
@@ -41,7 +54,7 @@ var chapter5 = [
       { 
         'number' : "3)",
         'timer' : 232,  
-        'text' : "The experiment is preformed again in order to make sense of the illogical results. Considering that P/E often exhibit properties of particles, the result is unexpected- if the experiment was performed with particles, the images on each board would be identical in shape (the particles would be fixed on board2 as if board1 were a stencil - [ || ]).",
+        'text' : "The experiment is preformed again in order to make sense of the illogical results. Considering that P/E often exhibit properties of particles, the result is unexpected- if the experiment was performed with particles, the images on each board would be identical in shape (the particles would be fixed on board2 as if board1 were a stencil.",
         'subtext' : "",
         'subtext2' : ""
        
@@ -60,7 +73,7 @@ var chapter5 = [
         'number' : "5)",
         'timer' : 272,  
         'text' : "The results of the experiment are found on the board2.",
-        'subtext' : "The diffraction pattern on board2 has been replaced with a replica of the two slits [ || ]. With a monitor present, the illogical simultaneous behavior of P/E is replaced with the originally expected behavior of particles. ",
+        'subtext' : "The diffraction pattern on board2 has been replaced with a replica of the two slits. With a monitor present, the illogical simultaneous behavior of P/E is replaced with the originally expected behavior of particles. ",
         'subtext' : "",
         'subtext2' : ""
 
@@ -81,7 +94,12 @@ var chapter5 = [
 
 
 
-
+function dart(){
+  
+  document.querySelector('.line5').style.width="20%";
+  document.querySelector('.line6').style.width="60%";
+  
+ };
 
 
 
