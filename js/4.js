edb65b9caaef504dc68bc632397fc28a41b5a976
Camila Mercado <camilamercado11@gmail.com>
2014-04-28 13:31:48 -0400

    change timing

--- js/4.js
+++ js/4.js
@@ -47,7 +47,7 @@ setInterval( function order(){
 
         }
 
-        if( time >= 260) {
+        if( time >= 149) {
 
           document.querySelector(".Laurel4").style.display="block";
 
@@ -91,64 +91,64 @@ var chapter = [
 
       {
         'number' : "1)", 
-        'timer' : 200,
+        'timer' : 101,
         'text' : "Two figures are hit by Cupids arrow. The god (Apollo), and the nymph (Daphne)."
          
       }, 
 
       { 
         'number' : "2)",
-        'timer' : 210,  
+        'timer' : 109,  
         'text' : "Apollo is struck by an unflinching resolve to possess Daphne. Daphne is struck by an unflinching resolve to evade Apollo."
 
       }, 
 
       { 
         'number' : "3)",
-        'timer' : 220,  
+        'timer' : 117,  
         'text' : "The two figures circle the planet earth in pursuit and evasion. At the end of a third orbit around the globe, Daphne begins to decelerate (feeling an entropy that gods are not acquainted with)."
 
       }, 
 
       { 
         'number' : "4)",
-        'timer' : 230,  
+        'timer' : 125,  
         'text' : "The decrease in Daphne's speed is a directly proportional inverse relationship to her increase in fear, and Apollo's increase in joy."
       }, 
 
       { 
         'number' : "5)",
-        'timer' : 240,  
+        'timer' : 133,  
         'text' : "As Daphne's speed/fear ratio continues to tip in favor of terror, she introduces a third figure into the equation, (her father the river god Peneus) by calling for help."     
       }, 
 
       { 
         'number' : "6)",
-        'timer' : 250,  
+        'timer' : 141,  
         'text' : "The gaze of Peneus pierces through levels of sediment, rock, water and flesh to fix upon the form of the terror stricken nymph."     
       }, 
 
       { 
         'number' : "7)",
-        'timer' : 260,  
+        'timer' : 149,  
         'text' : "The sprouting of ten fingertips is quickly followed by a hardening of the flesh, a rigidness of the hair, and the thick sappy gag of tree sap. "     
       }, 
 
       { 
         'number' : "8)",
-        'timer' : 270,  
+        'timer' : 157,  
         'text' : "Apollo looks over the transformation in devastating glee. While Daphne is no longer a nymph, she is now incapable of flight, and defenseless against the realization of Apollo's ever-present will to possess her as his own."     
       }, 
 
       { 
         'number' : "9)",
-        'timer' : 280,  
+        'timer' : 165,  
         'text' : "Time continues for Daphne, yet now in the form of tree rungs, and a god curled around her limbs. Daphne's figure is fixed in space, yet still desiring to flee."     
       }, 
 
       { 
         'number' : "10)",
-        'timer' : 290,  
+        'timer' : 173,  
         'text' : "Being so overjoyed at Daphne's newly pacified form, Apollo decides to commemorate his triumph by adopting the Laurel wreath as his personal symbol. He wears the token of Daphne's loss of mobility as an accessory to his power, invites a planets worth of new figures to do the same."     
       }, 
 
