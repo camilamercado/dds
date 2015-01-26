542ca2fde76bd48b1947fe6c0bbc175b8a8b502b
camila <camila@camilas-MacBook-Pro-2.local>
2015-01-26 12:21:31 -0500

    add invitation

--- js/bodies.js
+++ js/bodies.js
@@ -12,15 +12,15 @@ var bodies = [
 
 			{ 
 				'number' : "2)",
-				'timer' : 20,  
+				'timer' : 15,  
 				'img' : "url(./img/db.jpg)",
-	 			'btext' : "2) Oceanus - (grandfather of Daphne) Diety of a fresh water stream that circles the earth along the equator. The sun, moon and stars are all claimed to rise and fall in his waters."
+	 			'btext' : "2) If it was you're two feet, they might carry you somewhere closer to home. If that home was a patch of dirt left of a river circling the globe, you might keep on running. "
 			}, 
 
 
 			{ 
 				'number' : "3)",  
-				'timer' : 35,
+				'timer' : 30,
 				'img' : "url(./img/telwave.jpg)",
 	 			'btext' : "3) A satellite in Geostationary Earth Orbit (GEO) revolves around the earth in the plane of the equator once in 24 hours, maintaining precise synchronization with the earths rotation. -Introduction to Satellite Communication "
 			}
@@ -41,7 +41,7 @@ var events = [
 
       { 
         'number' : "2)",
-        'timer' : 60,  
+        'timer' : 55,  
         'text' : "Apollo is struck by an unflinching resolve to possess Daphne. Daphne is struck by an unflinching resolve to evade Apollo.",
         'opacity' : 0.1,
 
@@ -49,7 +49,7 @@ var events = [
 
       { 
         'number' : "3)",
-        'timer' : 70,  
+        'timer' : 60,  
         'text' : "The two figures circle the planet earth in pursuit and evasion. At the end of a third orbit around the globe, Daphne begins to decelerate (feeling an entropy that gods are not acquainted with).",
         'opacity' : 0.3,
 
@@ -57,49 +57,49 @@ var events = [
 
       { 
         'number' : "4)",
-        'timer' : 80,  
+        'timer' : 65,  
         'text' : "The decrease in Daphne's speed is a directly proportional inverse relationship to her increase in fear, and Apollo's increase in joy.",
         'opacity' : 0.4,
       }, 
 
       { 
         'number' : "5)",
-        'timer' : 90,  
+        'timer' : 70,  
         'text' : "As Daphne's speed/fear ratio continues to tip in favor of terror, she introduces a third figure into the equation, (her father the river god Peneus) by calling for help.",
         'opacity' : 0.5,     
       }, 
 
       { 
         'number' : "6)",
-        'timer' : 100,  
+        'timer' : 75,  
         'text' : "The gaze of Peneus pierces through levels of sediment, rock, water and flesh to fix upon the form of the terror stricken nymph.",
         'opacity' : 0.6,     
       }, 
 
       { 
         'number' : "7)",
-        'timer' : 110,  
+        'timer' : 80,  
         'text' : "The sprouting of ten fingertips is quickly followed by a hardening of the flesh, a rigidness of the hair, and the thick sappy gag of tree sap. ",
         'opacity' : 0.7,     
       }, 
 
       { 
         'number' : "8)",
-        'timer' : 120,  
+        'timer' : 85,  
         'text' : "Apollo looks over the transformation in devastating glee. While Daphne is no longer a nymph, she is now incapable of flight, and defenseless against the realization of Apollo's ever-present will to possess her as his own." ,
         'opacity' : 0.8,    
       }, 
 
       { 
         'number' : "9)",
-        'timer' : 130,  
+        'timer' : 90,  
         'text' : "Time continues for Daphne, yet now in the form of tree rungs, and a god curled around her limbs. Daphne's figure is fixed in space, yet still desiring to flee.",
         'opacity' : 0.9,     
       }, 
 
       { 
         'number' : "10)",
-        'timer' : 140,  
+        'timer' : 100,  
         'text' : "Being so overjoyed at Daphne's newly pacified form, Apollo decides to commemorate his triumph by adopting the Laurel wreath as his personal symbol. He wears the token of Daphne's loss of mobility as an accessory to his power, invites a planets worth of new figures to do the same.",
         'opacity' : 1,     
       }, 
